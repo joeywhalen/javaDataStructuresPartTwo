ee76d31dfab0caea41f35f60e5b77cfb8af01bd8
Joey Whalen <jtktwhalen@gmail.com>
2021-02-01 15:00:43 -0500

    update Main to create Array

--- src/javaDataStructuresPartTwo/Main.java
+++ src/javaDataStructuresPartTwo/Main.java
@@ -1,9 +1,41 @@
 package javaDataStructuresPartTwo;
 
+import java.util.ArrayList;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
+		ArrayList<Integer> addNumbers = new ArrayList<>();// create ArrayList
+		
+		addNumbers.add(1);// add integers to the ArrayList
+		addNumbers.add(5);
+		addNumbers.add(9);
+		addNumbers.add(10);
+		addNumbers.add(20);
+		
+		
+		
+		
+		double[ ] exampleArray = {1,5,6,5,4,1};
+
+        double maximum = exampleArray[0];
+
+        int index = 0;
+
+        for (int i = 1; i<exampleArray.length; i++){
+
+             if (exampleArray[ i ] > maximum) {
+
+                  maximum = exampleArray[ i ];
+
+                  index = i;
+
+             }
+
+        }
+
+        System.out.println(index);
 
 	}
 
